e2c9e77621d80103ec532f0c046c1e2a90789f18
ndamtruong2k <88503188+ndamtruong2k@users.noreply.github.com>
2022-01-16 22:13:56 +0700

    Delete Compression directory

--- Compression/dct2d.m
+++
@@ -1,8 +0,0 @@
-function Y = dct2d(mat)
-[Rows, Cols] = size(mat);
- for k = 1:Rows
-     dctx(k,:) = dct1d(mat(k,:));
- end
- for k = 1:Cols
-     Y(:,k) = dct1d(dctx(:,k)')';
- end
--- Compression/idct2d.m
+++
@@ -1,8 +0,0 @@
-function Y = idct2d(dctmat)
-[Rows, Cols] = size(dctmat);
- for k = 1:Cols
-     dcty(:,k) = idct1d(dctmat(:,k)')';
- end
- for k = 1:Rows
-     Y(k,:) = idct1d(dcty(k,:));
- end
